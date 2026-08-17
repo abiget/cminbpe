a38ea92c0dff2afa90114c11e507548cc7afc9c6
abiget <getachewante@gmail.com>
2026-08-17 16:39:02 +0200

    chore: update the build config

--- setup.py
+++ setup.py
@@ -4,16 +4,17 @@ ext_modules = [
     Extension(
         name="cminbpe._minbpe",  # put the binary in the cminbpe package
         sources=["cminbpe/_minbpe.c"],
+        include_dirs=["cminbpe"],
+        extra_compile_args=["-O3"],
     )
 ]
 
 setup(
     name="cminbpe",
     version="0.1.0",
-    description="C implementation of BPE tokenizer",
+    description="A minimal BPE tokenizer implementation with C backend for fast training and tokenization.",
     author="abiget",
     author_email="getachewante@gmail.com",
     packages=find_packages(include=["cminbpe"]),
     ext_modules=ext_modules,
-    cflags=["-O3", "-march=native"],
 )
